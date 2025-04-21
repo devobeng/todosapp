2ee7842fa785d3a51bca08a4c7c0142391e7deef
devobeng <dev.obeng.bismark@gmail.com>
2025-04-21 07:36:44 -0700

    added db connection mongodb Altlass

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const connectDb = require("./config/db");
+
+connectDb();
 
 app.use(cors());
 app.use(express.json());
